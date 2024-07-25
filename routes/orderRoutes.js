182252ba0e00e90424487f48afae8fde7cd62037
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2024-07-25 17:01:19 +0530

    order updated

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,5 +5,5 @@ const authMiddleware=require('../Middleware/auth')
 const orderRouter=express.Router()
 
 orderRouter.post('/order/placeOrder',authMiddleware,orderController.placeOrder)
-// orderRouter.post('/order/verify',orderController.verifyOrder)
+orderRouter.post('/order/verify',orderController.verifyOrder)
 module.exports=orderRouter;
