70bea0161e92e44e5da353f39c4a7a5fd83f4177
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2024-08-13 15:00:34 +0530

    created orderapi for admin

--- controller/orderContoller.js
+++ controller/orderContoller.js
@@ -89,4 +89,17 @@ const userOrders = async (req, res) => {
   }
 };
 
-module.exports = { placeOrder, verifyOrder, userOrders };
+
+// listing orders for admin panel
+ const listOrders=async(req,res)=>{
+  try {
+    const orders=await orderModel.find({})
+    res.json({success:true,data:orders})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+    
+  }
+ }
+
+module.exports = { placeOrder, verifyOrder, userOrders,listOrders };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -7,4 +7,6 @@ const orderRouter=express.Router()
 orderRouter.post('/order/placeOrder',authMiddleware,orderController.placeOrder)
 orderRouter.post('/order/verify',orderController.verifyOrder)
 orderRouter.post('/order/userorders',authMiddleware,orderController.userOrders)
+orderRouter.get('/order/userorderslist',orderController.listOrders)
+
 module.exports=orderRouter;
