8e7eb168b7b7140e4bfd020f366cbe2a5578e7ca
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2024-08-13 10:53:49 +0530

    new updated orderController

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,5 +6,5 @@ const orderRouter=express.Router()
 
 orderRouter.post('/order/placeOrder',authMiddleware,orderController.placeOrder)
 orderRouter.post('/order/verify',orderController.verifyOrder)
-orderRouter.post('/order/userorders',orderController.userOrders)
+orderRouter.post('/order/userorders',authMiddleware,orderController.userOrders)
 module.exports=orderRouter;
